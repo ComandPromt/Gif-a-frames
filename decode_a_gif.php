9cec53643f0eb5fb47a9c4277834690083ba1aea
ComandPromt <vayase_a_ferrol@hotmail.com>
2018-05-09 14:34:21 +0200

    Add files via upload

---
+++ decode_a_gif.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(0);
+    include_once ( str_replace('\\','/',dirname(__FILE__) ) ."/GIFDecoder.class.php" );
+
+	$animation = 'picture.gif';
+
+  	$gifDecoder = new GIFDecoder ( fread ( fopen ( $animation, "rb" ), filesize ( $animation ) ) );
+
+	$i = 1;
+	foreach ( $gifDecoder -> GIFGetFrames ( ) as $frame ) {
+		if ( $i < 10 ) {
+			fwrite ( fopen ( "frames/frame0$i.png" , "wb" ), $frame );
+		}
+		else {
+			fwrite ( fopen ( "frames/frame$i.png" , "wb" ), $frame );
+		}
+		$i++;
+	}
+?>
